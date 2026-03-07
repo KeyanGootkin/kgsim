4821d05c7f15d3ac9bd62bd5fbf7f29733752678
Keyan <keyan@dhcp89.ifa.hawaii.edu>
2026-03-06 16:09:25 -1000

    move fields to a submodule

---
+++ src/kgsim/fields/calc.py
@@ -0,0 +1,109 @@
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+# >-|===|>                             Imports                             <|===|-<
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+from kgsim.fields.scalar import ScalarField
+from kgsim.fields.vector import VectorField
+from kbasic.typing import ArrayLike, Number 
+from numpy import gradient, cumsum, ndarray, inf
+from numpy.typing import NDArray
+from typing import Optional
+
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+# >-|===|>                            Functions                            <|===|-<
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+def ddx(F: ArrayLike, dx: Number) -> NDArray: return gradient(F, dx)[-1]
+def ddy(F: ArrayLike, dy: Number) -> NDArray: return gradient(F, dy)[-2] 
+def ddz(F: ArrayLike, dz: Number) -> NDArray: return gradient(F, dz)[-3]
+def intdx(F: ArrayLike, dx: Number) -> NDArray: return cumsum(F, axis=1) * dx
+def intdy(F: ArrayLike, dy: Number) -> NDArray: return cumsum(F, axis=0) * dy
+
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+# >-|===|>                             Classes                             <|===|-<
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+class NablaOperator:
+    __ins = None
+    def __new__(cls):
+        if cls.__ins is None:
+            cls.__ins = super().__new__(cls)
+        return cls.__ins
+    def parse_grid_di(self, Field, di=None) -> tuple:
+        match di:
+            case _ if type(di) in ArrayLike.types: return tuple(di)
+            case _ if type(di) in Number.types: return (di, di, di) 
+            case None:
+                match Field:
+                    case ndarray(): pass 
+                    case ScalarField(parent=None): return (1, 1, 1)
+                    case ScalarField(ndims=3): return (Field.parent.dz, Field.parent.dy, Field.parent.dx)
+                    case ScalarField(ndims=2): return (Field.parent.dy, Field.parent.dx)
+                    case VectorField(): return self.parse_grid_di(Field.x, di=di)
+    def __mul__(
+            self, 
+            other: VectorField, 
+            di: Optional[Number|ArrayLike] = None
+            ) -> NDArray: # divergence
+        match other:
+            case VectorField():
+                d = self.parse_grid_di(other.x)
+                dz,dy,dx = d if len(d)==3 else tuple([inf, *d])
+                dFxdx = ddx(other.x[:], dx)
+                dFydy = ddy(other.y[:], dy)
+                dFzdz = ddz(other.z[:], dz) if other.ndims==3 else 0
+                return dFxdx + dFydy + dFzdz
+    def x(
+            self, 
+            other: VectorField | NDArray
+            ) -> VectorField | NDArray: # curl
+        match other:
+            case VectorField(): 
+                d = self.parse_grid_di(other.x)
+                dz,dy,dx = d if len(d)==3 else tuple([inf, *d])
+                dt = 1 if not other.parent else other.parent.dt
+                dFzdy = ddy(other.z[:], dy)
+                dFydz = ddz(other.y[:], dz) if other.ndims==3 else 0 
+                dFxdz = ddz(other.x[:], dz) if other.ndims==3 else 0 
+                dFzdx = ddx(other.z[:], dx) 
+                dFydx = ddx(other.y[:], dx)
+                dFxdy = ddy(other.x[:], dy)
+                return VectorField(
+                    dFzdy - dFydz,
+                    dFxdz - dFzdx,
+                    dFydx - dFxdy,
+                    caching=other.caching, verbose=other.verbose, parent=other.parent, 
+                    name=f"Curl of {other.name}", latex=fr"$\Nabla \times {other.latex}"
+                )
+            case ndarray():
+                d = self.parse_grid_di(other, di)
+                dz,dy,dx = d if len(d)==3 else tuple([inf, *d])
+                dFzdy = ddy(other[-3], dy) if len(d)==3 else 0
+                dFydz = ddz(other[-2], dz) if len(d)==3 else 0 
+                dFxdz = ddz(other[-1], dz) if len(d)==3 else 0 
+                dFzdx = ddx(other[-3], dx) if len(d)==3 else 0
+                dFydx = ddx(other[-2], dx)
+                dFxdy = ddy(other[-1], dy)
+                return array([
+                    dFydx - dFxdy,
+                    dFxdz - dFzdx,
+                    dFzdy - dFydz
+                ]) if len(d)==3 else dFydx - dFxdy
+    def __call__(self, *args, **kwds) -> VectorField: # gradient
+        match args:
+            case (ScalarField(),): 
+                F: ScalarField = args[0]
+                arr: ndarray = F[:]
+                di: tuple[float] = F.di if 'di' not in kwds.keys() else kwds['di']
+                gradarr: ndarray = gradient(arr, di)
+                # assume that the 0th axis is time, as will be the case with all simulation outputs, but not necessarily numpy arrays
+                # assume that the last axis is x
+                return VectorField(
+                    *gradarr[:0:-1],
+                    caching=F.caching, verbose=F.verbose, name="grad-"+F.name, latex=fr"$\Nabla {F.latex[1:]}", parent=F.parent
+                )
+            case _: print("bad arguments : "+str(args)+str(kwds))
+
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+# >-|===|>                           Definitions                           <|===|-<
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+Nabla = NablaOperator()
+Del = NablaOperator()
+Grad = NablaOperator()
---
+++ src/kgsim/fields/vector.py
@@ -0,0 +1,186 @@
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+# >-|===|>                             Imports                             <|===|-<
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+from kgsim.fields.scalar import ScalarField
+from kbasic.typing import ArrayLike
+from kbasic.bar import verbose_bar
+from kplot import default_cmap, show_video
+from functools import cached_property
+from typing import Optional, Self
+from numpy.typing import NDArray
+from numpy import array, sqrt, zeros, nanstd, arange, cumsum, hypot
+from matplotlib.pyplot import gca 
+from matplotlib.axes import Axes
+
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+# >-|===|>                            Functions                            <|===|-<
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+def Az(Bx, By, dx=1, dy=1):
+    Az = zeros(Bx.shape)
+    Az[1:] = cumsum(Bx[1:]*dy, axis=0)
+    Az[:,1:] = (Az[:,0]-cumsum(By[:,1:]*dx, axis=1).T).T
+    return Az
+
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+# >-|===|>                             Classes                             <|===|-<
+# !==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==!==
+class VectorField:
+    def __init__(
+            self, 
+            *components, 
+            caching: bool = False, 
+            verbose: bool = False,
+            name: Optional[str] = None, 
+            latex: Optional[str] = None,
+            parent = None,
+            parallel: Optional[str] = 'z'
+        ) -> None:
+        latex = "".join([c for c in latex if c not in r"$\{}"])
+        self.name = name
+        self.latex = latex 
+        self.parent = parent
+        if parent: 
+            self.dx, self.dy = parent.dx, parent.dy
+        self.verbose = verbose 
+        self.caching = caching
+        child_kwargs = {'parent':parent, 'verbose':verbose, 'caching':caching}
+        if len(components)==1 and type(path:=components[0])==str:
+            components = (
+                ScalarField(path+"/x", name=name+"_x_component", latex=f"${latex}_x$", **child_kwargs), 
+                ScalarField(path+"/y", name=name+"_y_component", latex=f"${latex}_y$", **child_kwargs), 
+                ScalarField(path+"/z", name=name+"_z_component", latex=f"${latex}_z$", **child_kwargs)
+            )
+        self.ndims = len(components)
+        assert 1<self.ndims<4, f"Only 2-3 components are supported. {len(components)} were given"
+        component_names = "xyz"
+        self.components = []
+        for name,val in zip(component_names, components): 
+            comp = ScalarField(val, caching=self.caching) if type(val)==str else val
+            self.components.append(comp)
+            setattr(self, name, comp)
+        self.size = sum([c.size for c in self.components])
+        self.set_parallel(parallel)
+    def __len__(self) -> int: return min([len(self.x), len(self.y), len(self.z)])
+    def __abs__(self) -> NDArray:
+        homo = all([len(self.x[i])==len(self.x[0]) for i in range(len(self))])
+        return array([
+            sqrt(sum([
+                c[i]**2 for c in self.components
+            ])) for i in verbose_bar(range(len(self)), self.verbose, desc="taking magnitude...")
+        ], dtype=float if homo else object)
+    def __getitem__(self, item: int|slice) -> NDArray:
+        match type(item):
+            case int(): return array([c[item] for c in self.components])
+            case slice():
+                item_iters = [
+                    i for i in range(
+                        item.start if not item.start is None else 0, 
+                        item.stop if not item.stop is None else len(self), 
+                        item.step if not item.step is None else 1
+                    )
+                ]
+                return array([
+                    [
+                        c[i] for c in self.components
+                    ] for i in item_iters
+                ])
+    def dot(self, other: Self) -> NDArray: 
+        if isinstance(other, VectorField):
+            assert self.ndims==3, "only 3D vector fields can be dotted at this time"
+            assert other.ndims==3, "Can only dot into a 3D vector field at this time"
+            if self.verbose: print("constructing A...")
+            A = array([
+                [
+                    [
+                        [self.x[k][i,j], self.y[k][i,j], self.z[k][i,j]]
+                    for i in range(self.shape[0])]
+                for j in range(self.shape[1])] 
+            for k in verbose_bar(range(len(self)), self.verbose, desc="constructing A...")])
+            B = array([
+                [
+                    [
+                        [other.x[k][i,j], other.y[k][i,j], other.z[k][i,j]]
+                    for i in range(other.shape[0])]
+                for j in range(other.shape[1])] 
+            for k in verbose_bar(range(len(other)), self.verbose, desc="constructing B...")])
+            return sum(A * B, axis=2)
+    def cross(self, other: Self, k:int) -> NDArray:
+        if type(other)==VectorField: 
+            assert self.ndims==3, "only 3D vector fields can be crossed at this time"
+            assert other.ndims==3, "Can only cross into a 3D vector field at this time"
+            return array([
+                self.y[k]*other.z[k] - self.z[k]*other.y[k], 
+                self.z[k]*other.x[k] - self.x[k]*other.z[k],
+                self.x[k]*other.y[k] - self.y[k]*other.x[k]
+            ])
+    @cached_property
+    def potential(self) -> NDArray:
+        match len(self.x.shape):
+            case 2: 
+                return array([Az(self.x[i], self.y[i], dx=self.dx, dy=self.dy) for i in range(len(self))])
+    def calc_perp(self, item=None) -> NDArray: 
+        if not item:
+            self.perp = array([
+                hypot(self.perpendicular[0][j], self.perpendicular[1][j]) 
+                for j in verbose_bar(range(len(self)), self.verbose, desc="perpendicularizing")
+            ])
+        elif type(item)==int: 
+            self.perp = hypot(self.perpendicular[0][item], self.perpendicular[1][item])
+        elif type(item)==slice: 
+            item_iters = [
+                i for i in range(
+                    item.start if item.start else 0,
+                    item.stop if item.stop else len(self),
+                    item.step if item.step else 1
+                )
+            ]
+            self.perp = array([hypot(self.perpendicular[0][j], self.perpendicular[1][j]) for j in item_iters])
+        else: raise TypeError(f"calc_perp only takes ints, slices, or None for item, not {type(item)}-type objects")
+    def set_parallel(self, component:str) -> None:
+        match component.lower():
+            case 'x':
+                self.parallel = self.x 
+                self.perpendicular = self.y, self.z 
+            case 'y':
+                self.parallel = self.y 
+                self.perpendicular = self.x, self.z 
+            case 'z':
+                self.parallel = None if not hasattr(self, 'z') else self.z
+                self.perpendicular = self.x, self.y 
+    def movie(self, mode='mag', norm='none', cmap=default_cmap, **kwrg) -> None:
+        match mode.lower():
+            case 'mag'|'magnitude'|'abs':
+                @show_video(name=self.name+"_magnitude", latex=fr"$|{self.name}|$", norm=norm, cmap=cmap)
+                def reveal_thyself(s, **kwargs): return abs(self)
+            case 'perp'|'perpendicular':
+                @show_video(name=self.name+"_perp", latex=fr"${self.name}_\perp$", norm=norm, cmap=cmap)
+                def reveal_thyself(s, **kwargs): return self.perp                
+            case 'par'|'parallel':
+                @show_video(name=self.name+"_par", latex=fr"${self.name}_\parallel$", norm=norm, cmap=cmap)
+                def reveal_thyself(s, **kwargs): return array([self.parallel[i] for i in range(len(self))])
+        reveal_thyself(self if self.parent is None else self.parent, **kwrg)
+    def quiver(
+        self,
+        ind: int,
+        ax: Optional[Axes] = None,
+        #x/y axes
+        density: float = 10,
+        x: Optional[NDArray] = None,
+        y: Optional[NDArray] = None,
+        transpose: bool = False,
+        #everything else goes into matplotlib command
+        **kwargs
+    ) -> :
+        if not ax: ax = gca()
+        match x, y:
+            case None, None:
+                dx = int(self.x[0].shape[1] // density)
+                x = arange(0, self.x[0].shape[1], dx)
+                dy = int(self.y[0].shape[0] // density)
+                y = arange(0, self.x[0].shape[0], dy)
+        # plot data
+        print(dx, dy)
+        if not transpose:
+            ax.quiver(x, y, self.y[ind][::dy,::dx].T, self.x[ind][::dy,::dx].T, **kwargs)
+        else: 
+            ax.quiver(y, x, self.x[ind][::dy,::dx], self.y[ind][::dy,::dx], **kwargs)
